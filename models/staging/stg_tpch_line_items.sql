82966f3de591c3f3a1a3e8adfb99fe9ae96b0c1d
panashe Nash masocha <nashmasocha@outlook.com>
2026-02-07 22:13:26 +0200

    Transformed models (fact tables, data marts

--- models/staging/stg_tpch_line_items.sql
+++ models/staging/stg_tpch_line_items.sql
@@ -4,13 +4,13 @@ select
             'l_orderkey',
             'l_linenumber'
         ])
-    }} as line_item_key,
-    l_orderkey as order_key,
-    l_linenumber as line_number,
-    l_partkey as part_key,
-    l_quantity as quantity,
-    l_extendedprice as extended_price,
-    l_discount as discount_percentage,
-    l_tax as tax_rate,
-from 
-    {{ source('tpch','lineitem') }}
+    }} as order_item_key,
+	l_orderkey as order_key,
+	l_partkey as part_key,
+	l_linenumber as line_number,
+	l_quantity as quantity,
+	l_extendedprice as extended_price,
+	l_discount as discount_percentage,
+	l_tax as tax_rate
+from
+    {{ source('tpch', 'lineitem') }}
